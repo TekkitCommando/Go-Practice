c114c64ce9fa2ce4623738fb9dca68ad74f6f008
Ethan Smith <ethan.smith1845@gmail.com>
2017-02-28 16:39:20 -0600

    Update scope a little

--- scope/package_scope.go
+++ scope/package_scope.go
@@ -1,4 +1,4 @@
-package packagescope
+package scope
 
 import "fmt"
 
@@ -7,6 +7,8 @@ var x int = 42 // Package level scope (Accessible only in its own package)
 func main() {
 	fmt.Println(x)
 	foo()
+	y := 17 // Block level scope (Accessible only down to the curly brace)
+	fmt.Println(y)
 }
 
 func foo() {
