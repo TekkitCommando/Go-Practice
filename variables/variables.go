4dd5aee7e20a7658f1ceff804eee1b434039fe2d
Ethan Smith <ethan.smith1845@gmail.com>
2017-02-28 07:37:17 -0600

    Add more comments

--- variables/variables.go
+++ variables/variables.go
@@ -9,17 +9,17 @@ func main() {
 	money := 19.99
 	isTwenty := false
 
-	// Var notation, this is the other way to do variables. Using more then just these two can affect readablility
+	// Var notation, this is the other way to do variables. Using more than just these two can affect readablility
 	var varName string = "Ethan"
 	var varAge int = 19
 	var varMoney float64 = 19.99 // There is also float32
 	var varIsTwenty bool = false
 
-	fmt.Printf("%v \n", name)
+	fmt.Printf("%v \n", name) // Prints the value (Ex. "Ethan")
 	fmt.Printf("%v \n", age)
 	fmt.Printf("%v \n", money)
 	fmt.Printf("%v \n", isTwenty)
-	fmt.Printf("%T \n", varName)
+	fmt.Printf("%T \n", varName) // Prints what type the value is (Ex. bool or string)
 	fmt.Printf("%T \n", varAge)
 	fmt.Printf("%T \n", varMoney)
 	fmt.Printf("%T \n", varIsTwenty)
