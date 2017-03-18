773c1d66840fdc54c13fdeb97ff6cfbe98697caf
Ethan Smith <ethan.smith1845@gmail.com>
2017-03-18 13:42:24 -0500

    Add a struct example

---
+++ data_structures/structs.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last string
+	age int
+}
+
+type teacher struct {
+	person
+	canTeach bool
+}
+
+func (p person) fullName() string {
+	return p.first + p.last
+}
+
+func main() {
+	p1 := teacher{
+		person: person{"Ethan", "Smith", 19},
+		canTeach: true,
+	}
+	p2 := teacher{
+		person: person{"John", "Doe", 20},
+		canTeach: false,
+	}
+	fmt.Println(p1.fullName(), p1.canTeach)
+	fmt.Println(p2.fullName(), p2.canTeach)
+}
