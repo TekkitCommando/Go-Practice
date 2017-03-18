c143c00d4e32b8099fe4d80bac203c1eca3381ac
Ethan Smith <ethan.smith1845@gmail.com>
2017-03-18 14:32:30 -0500

    Add json encode/decode

---
+++ json/encode.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Person struct {
+	First string
+	Last string
+	Age int
+	notExported int
+}
+
+// Encodes to a stream, useful for web services
+func main() {
+	p1 := Person{"James", "Bond", 20, 007}
+	json.NewEncoder(os.Stdout).Encode(p1)
+}
